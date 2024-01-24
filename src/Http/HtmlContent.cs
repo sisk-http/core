c85fda634d2644f50571a9b890e0586322163766
CypherPotato <gab@proj.pw>
2024-01-24 12:02:51 -0300

    fix htmlcontent not sending charset parameter

--- src/Http/HtmlContent.cs
+++ src/Http/HtmlContent.cs
@@ -13,7 +13,7 @@ using System.Text;
 namespace Sisk.Core.Http;
 
 /// <summary>
-/// Provides HTTP content based on HTML file.
+/// Provides HTTP content based on HTML contents.
 /// </summary>
 /// <definition>
 /// public class HtmlContent : ByteArrayContent
@@ -45,9 +45,10 @@ public class HtmlContent : ByteArrayContent
     /// <type>
     /// Constructor
     /// </type>
-    public HtmlContent(string content, Encoding? encoding) : base(GetContentBytes(content, encoding))
+    public HtmlContent(string content, Encoding encoding) : base(GetContentBytes(content, encoding))
     {
         Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/html");
+        Headers.ContentType.CharSet = encoding.HeaderName;
     }
 
     /// <summary>
@@ -63,7 +64,7 @@ public class HtmlContent : ByteArrayContent
     public HtmlContent(string content) : this(content, DefaultEncoding) { }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static byte[] GetContentBytes(string content, Encoding? encoder)
+    private static byte[] GetContentBytes(string content, Encoding encoder)
     {
         ArgumentNullException.ThrowIfNull(content, nameof(content));
         return (encoder ?? Encoding.UTF8).GetBytes(content);
